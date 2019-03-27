6a2a6bac63ee57434407585890ece8f147d85c30
luguco <syant@web.de>
2019-03-27 08:46:34 +0100

    Add files via upload
    added map, messages file

--- Object.py
+++ Object.py
@@ -1,9 +1,10 @@
 class Object(object):
-    def __init__(self, pname, pid, pmovable, pinv):
+    def __init__(self, pname, pid, pmovable, ppickable, pinv):
         self.name = pname
         self.id = pid
         self.movable = pmovable
         self.inventory = pinv
+        self.pickable = ppickable
 
     def getName(self):
         return self.name
@@ -13,6 +14,9 @@ class Object(object):
 
     def isMovable(self):
         return self.movable
+
+    def isPickable(self):
+        return self.pickable
 
     def getInventory(self):
         return self.inventory
